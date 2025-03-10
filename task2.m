92d5ac6d6df9a077ccd8e9229067a8f735a2ecb2
BasantSamy22 <152982931+BasantSamy22@users.noreply.github.com>
2025-03-10 15:43:07 +0200

    Update task2.m

--- task2.m
+++ task2.m
@@ -1,14 +1,12 @@
 % Numerical Solution of ODE using Euler Methods
 
-% Parameters
-t0 = 0;      % Initial time
-tf = 2;      % Final time
-y0 = 1;      % Initial condition
-h = 0.04;    % Step size
-t = t0:h:tf; % Time vector
+t0 = 0;      
+tf = 2;     
+y0 = 1;      
+h = 0.04;    
+t = t0:h:tf; 
 N = length(t);
 
-% Initialize solutions
 y_forward = zeros(1, N);
 y_modified = zeros(1, N);
 y_backward = zeros(1, N);
@@ -26,7 +24,7 @@ for i = 1:N-1
     y_forward(i+1) = y_forward(i) + h * (-50 * y_forward(i) + sin(t(i)));
 end
 
-% Modified Euler Method (Heun's Method)
+% Modified Euler Method 
 for i = 1:N-1
     y_pred = y_modified(i) + h * (-50 * y_modified(i) + sin(t(i))); % Predictor Step
     y_modified(i+1) = y_modified(i) + (h/2) * ((-50 * y_modified(i) + sin(t(i))) + (-50 * y_pred + sin(t(i+1)))); % Corrector Step
@@ -37,19 +35,17 @@ for i = 1:N-1
     y_backward(i+1) = (y_backward(i) + h * sin(t(i+1))) / (1 + 50 * h);
 end
 
-% Runge-Kutta 4th Order (RK4)
+% Runge-Kutta 
 for i = 1:N-1
     y_rk4(i+1) = y_rk4(i) + h * (-50 * y_rk4(i) + sin(t(i))) + (h/2) * (-50 * (y_rk4(i) + h * (-50 * y_rk4(i) + sin(t(i)))) + sin(t(i) + h));
 end
 
-% Adams-Bashforth 2-step (AB2)
-% First step uses Euler method
+% Adams-Bashforth 
 y_ab2(2) = y_ab2(1) + h * (-50 * y_ab2(1) + sin(t(1)));
 for i = 2:N-1
     y_ab2(i+1) = y_ab2(i) + (h/2) * (3 * (-50 * y_ab2(i) + sin(t(i))) - (-50 * y_ab2(i-1) + sin(t(i-1))));
 end
 
-% Plot Results
 figure;
 plot(t, y_forward, 'r', 'LineWidth', 1.5); hold on;
 plot(t, y_modified, 'b', 'LineWidth', 1.5);
